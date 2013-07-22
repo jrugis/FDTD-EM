5091b8a6a4b66d4f8472529210ca309c51410bd7
jrugis <jrugis@gmail.com>
2013-07-22 22:23:10 +1200

    Edited license file.

--- utils.cpp
+++ utils.cpp
@@ -2,7 +2,7 @@
 GL_10
 An OpenGL+Qt4 FDTD electromagnetic simulation & visualization program.
 
-Copyright (C) 2005-2012 John Rugis
+Copyright (C) 2005-2013 John Rugis
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
